eeabbcf1b6c8b26f7d7c0f6531e0721b02c72caa
Saad Sayyed <saadsyed950@gmail.com>
2026-02-19 20:04:13 +0530

    Mobile app using nativewind

---
+++ mobile-app/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-white/70 flex-1 justify-center items-center w-full">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
